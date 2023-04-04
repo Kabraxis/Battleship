a2211316291e726ade56dc90466ae1d05506205a
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-04-04 18:34:11 +0200

    update

--- Topics/NPE/Avoid NPE with strings/src/Main.java
+++ Topics/NPE/Avoid NPE with strings/src/Main.java
@@ -1,13 +1,14 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 
 public class Main {
 
     public static String concatStrings(String str1, String str2) {
-        Objects.equals(str1, str2)
+        return str1 != null && str2 != null ? str1.concat(str2) :
+                str1 != null ? str1 :
+                        str2 != null ? str2 : "";
 
-        return str1.concat(str2);
+
     }
 
     /* Do not change code below */
